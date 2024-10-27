572ebaf9deaff8564815dad0b0b9c9cd84649a2d
tsuchida-yk <jwi6m4few4@yahoo.co.jp>
2024-10-27 04:45:22 +0000

    laravel16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -32,13 +32,43 @@ class ProfileController extends Controller
         // 追記ここまで
         return redirect('admin/profile/create');
     }
-    public function edit()
+
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-    public function update()
+
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+
+        if ($request->remove == 'true') {
+            $profile_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $profile_form['image_path'] = basename($path);
+        } else {
+            $profile_form['image_path'] = $profile->image_path;
+        }
+
+        unset($profile_form['image']);
+        unset($profile_form['remove']);
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+
+        return redirect('admin/profile');
     }
 }
 
